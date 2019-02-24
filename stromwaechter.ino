10f3e74394ba4e9aba0da9876228885d9c4ba49e
Bernhard Mayer <bernhard@generationmake.de>
2019-02-24 14:08:11 +0100

    added useful description to the header, changed update interval to 10 seconds

--- stromwaechter.ino
+++ stromwaechter.ino
@@ -1,25 +1,14 @@
 /*
- Basic ESP8266 MQTT example
+ * 
+ * Software for the stromwaechter pcb
+ * http://stromwaechter.generationmake.de
+ * Bernhard Mayer, DL1MAB, bernhard@generationmake.de
+ * 2019-02-24
 
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
+ 
+ Based on the Basic ESP8266 MQTT example of the PubSubClient library.
 
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
+it reads all the sensors and sends the status every 10 seconds.
 
 */
 
@@ -307,7 +296,7 @@ void loop() {
   client.loop();
 
   long now = millis();
-  if (now - lastMsg > 2000) {
+  if (now - lastMsg > 10000) {
   if(DS18B20.getDeviceCount()>0)
   {
     do {
